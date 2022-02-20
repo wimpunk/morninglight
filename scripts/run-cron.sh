81766462c1ec6441e5bf6839127f02efb5d5ba64
Wim Vinckier <wim.vinckier@24sea.eu>
2022-02-20 10:57:40 +0100

    run-cron: make it possible to run it in test

--- scripts/run-cron.sh
+++ scripts/run-cron.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-if [ "$1" = "init" ]; then
+if [ "$1" == "init" ]; then
 	$HOME/run-led.sh init
 	exit
 fi
 
 FILE=/tmp/state
 
-if test -r ${FILE}
-then STATE=`cat ${FILE}`
-else STATE=MORNING
-fi
+if [ "$1" == "test" ]; then
+	if test -r ${FILE}
+	then STATE=`cat ${FILE}`
+	else STATE=MORNING
+	fi
 
-case "$STATE" in
-	"MORNING") STATE=DAY
-		;;
-	"DAY") STATE=NIGHT
-		;;
-	"NIGHT") STATE=MORNING
-		;;
-esac
+	case "$STATE" in
+		"MORNING") STATE=DAY
+			;;
+		"DAY") STATE=NIGHT
+			;;
+		"NIGHT") STATE=MORNING
+			;;
+	esac
+else
+	STATE=$($HOME/fetch-part-of-day.sh)
+fi
 
 echo ${STATE}>${FILE}
 
